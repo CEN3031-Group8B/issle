ffaa129abe9433371418ab3bc19b6bb26cbe2dba
Mark Steele <steelram62@gmail.com>
2016-04-21 12:38:08 -0400

    removede extra files

--- modules/standards/server/routes/standards.server.routes.js
+++ modules/standards/server/routes/standards.server.routes.js
@@ -2,9 +2,6 @@
 
 module.exports = function(app) {
 	var standards = require('../controllers/standards.server.controller');
-	var countiesandschools = require('../controllers/countiesandschools.server.controller');
-	var counties = require('../controllers/counties.server.controller');
-	var schools = require('../controllers/schools.server.controller');
 	var standardsPolicy = require('../policies/standards.server.policy');
 
 	// Standards Routes
@@ -15,40 +12,10 @@ module.exports = function(app) {
 		.get(standards.read)
 		.put(standards.update)
 		.delete(standards.delete);
-	//countiesandschools routes	
-	app.route('/api/countiesandschools').all()
-		.get(countiesandschools.list)
-		.post(countiesandschools.create);
-
-	app.route('/api/countiesandschools/:countyId').all()
-		.get(countiesandschools.read)
-		.put(countiesandschools.update)
-		.delete(countiesandschools.delete);
-	//counties routes
-	app.route('/api/counties').all()
-		.get(counties.list)
-		.post(counties.create);
-
-	app.route('/api/counties/:countiesId').all()
-		.get(counties.read)
-		.put(counties.update)
-		.delete(counties.delete);
-	//schools routes (has county in it)
-	app.route('/api/schools').all()
-		.get(schools.list)
-		.post(schools.create);
-
-	app.route('/api/schools/:schoolsId').all()
-		.get(schools.read)
-		.put(schools.update)
-		.delete(schools.delete);
-	
 	
 	
 
 	// Finish by binding the Standard middleware
 	app.param('standardId', standards.standardByID);
-	app.param('countyId', countiesandschools.countyByID);
-	app.param('countiesId', counties.countiesByID);
-	app.param('schoolsId', schools.schoolsByID);
+
 };
--- modules/users/client/controllers/authentication.client.controller.js
+++ modules/users/client/controllers/authentication.client.controller.js
@@ -1,14 +1,15 @@
 'use strict';
 
-angular.module('users').controller('AuthenticationController', ['$scope', '$state', '$http', '$location', '$window', 'Authentication', 'CountiesAndSchools', 'Schools', 'Counties',
-  function ($scope, $state, $http, $location, $window, Authentication, CountiesAndSchools, Schools, Counties) {
+angular.module('users').controller('AuthenticationController', ['$scope', '$state', '$http', '$location', '$window', 'Authentication', 'Schools', 
+  function ($scope, $state, $http, $location, $window, Authentication, Schools) {
     $scope.authentication = Authentication;
     // Get an eventual error defined in the URL query string:
 	$scope.error = $location.search().err;
-	$scope.countiesandschools = CountiesAndSchools.query();
+	
+	//get list of schools
 	$scope.schools = Schools.query();
 	$scope.schoolsC=[];
-	$scope.counties = Counties.query();
+	//set school list depending on dropdown choices
 	$scope.countyChange = function(){
 		$scope.schoolsC = Schools.query({county:$scope.credentials.county.trim()});
 
@@ -42,46 +43,6 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$stat
       });
     };
 	
-	/*
-	Created my own lists manually just for testing purposes
-	*/
-	
-	$scope.countyList = ['None','Alachua', 'Broward'];
-	$scope.schoolList=[];
-	$scope.county0=['None0','None1','None2'];
-	$scope.county1=['University of Florida', 'Sante Fe'];
-	$scope.county2=['Mater','Lakes','Stirrup'];
-	$scope.county='';
-	$scope.school='';
-	
-	$scope.County = function(){
-		$scope.schoolList=[];
-		$scope.credentials.county=$scope.county;
-		if($scope.county.trim()===$scope.countyList[1]){
-			$scope.schoolList=[];
-			for(var i=0;i<$scope.county1.length;i++){
-				$scope.schoolList.push($scope.county1[i]);
-			}
-			//$scope.school=$scope.county1[0];
-		}
-		else if($scope.county.trim()===$scope.countyList[2]){
-			$scope.schoolList=[];
-			for(var i=0;i<$scope.county2.length;i++){
-				$scope.schoolList.push($scope.county2[i]);
-			}
-			//$scope.school=$scope.county2[0];
-		}
-		else{
-			$scope.schoolList=[];
-			for(var i=0;i<$scope.county0.length;i++){
-				$scope.schoolList.push($scope.county0[i]);
-			}
-			//$scope.school=$scope.county0[0];
-		}
-		console.log($scope.school);
-	};
-	
-
     // OAuth provider request
     $scope.callOauthProvider = function (url) {
       var redirect_to;
